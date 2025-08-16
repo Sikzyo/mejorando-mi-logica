827e32d671d04fc9a58d2661d0b907911dbd750e
zoe <183669693+Sikzyo@users.noreply.github.com>
2025-08-16 18:00:13 -0500

    fix: corrección en el nombre de los archivos

---
+++ Cursos/Curso Python Midudev/Ejercicios °2/ejercicio_2.py
@@ -0,0 +1,9 @@
+###
+# Pide al usuario dos números y una operación (+, -, \*, /). Realiza la operación y muestra el resultado (maneja la división entre cero).
+###
+
+number_1 = float(input("Ingresa un numero para realizar una operación: "))
+operator = input("Ingresa un operador (+, -, *, /)")
+number_2 = float(input("Ingresa el segundo numero para realizar una operación: "))
+
+    
--- Cursos/Curso Python Midudev/Ejercicios °2/ejercico_2.py
+++
@@ -1,3 +0,0 @@
-###
-# Pide al usuario dos números y una operación (+, -, \*, /). Realiza la operación y muestra el resultado (maneja la división entre cero).
-###
