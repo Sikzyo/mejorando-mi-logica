7ba95196aa6a5037a7fa6135216429e456048358
zoe <183669693+Sikzyo@users.noreply.github.com>
2025-08-19 19:06:41 -0500

    feat: ejercicio numero 3 solucionado

---
+++ Cursos/Curso Python Midudev/Ejercicios °3/ejercicio_3.py
@@ -0,0 +1,17 @@
+###
+# Dada las siguientes listas:
+# 
+# pan = ["pan arriba"]
+# ingredientes = ["jamón", "queso", "tomate"]
+# pan_abajo = ["pan abajo"]
+#
+#Crea una lista llamada sandwich que contenga el pan de arriba, los ingredientes y el pan de abajo, en ese orden.
+###
+
+pan = ["pan arriba"]
+ingredientes = ["jamón", "queso", "tomate"]
+pan_abajo = ["pan abajo"]
+
+sandwich = pan + ingredientes + pan_abajo
+
+print(sandwich)
