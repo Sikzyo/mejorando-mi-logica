334f1289889d82faf80b873a346c03170eb3ac20
zoe <183669693+Sikzyo@users.noreply.github.com>
2025-08-19 22:11:15 -0500

    feat: ejercicio numero 4 solucionado

---
+++ Cursos/Curso Python Midudev/Ejercicios °3/ejercicio_4.py
@@ -0,0 +1,11 @@
+###
+# Dada una lista:
+# lista = [1, 2, 3]
+# Crea una nueva lista que contenga los elementos de la lista original duplicados. Ejemplo: [1, 2, 3] -> [1, 2, 3, 1, 2, 3]
+###
+
+lista = [1, 2, 3]
+
+new_lista = lista + lista
+
+print(new_lista)
